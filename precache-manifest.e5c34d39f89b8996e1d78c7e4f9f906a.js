80d4649f9a60049596d25ff337a77d279f008a2b
ricardo quintero <ricardo.quintero@schibsted.com>
2019-09-21 22:18:53 +0300

    Updates

---
+++ precache-manifest.e5c34d39f89b8996e1d78c7e4f9f906a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f4cfacf7ed734a1a8076870a4b75fc",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "41fe6da486a1b19f895f",
+    "url": "/todo-list/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/todo-list/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "41fe6da486a1b19f895f",
+    "url": "/todo-list/static/js/main.4663bbb7.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
